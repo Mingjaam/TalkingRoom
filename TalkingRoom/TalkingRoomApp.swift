c9d7180bffc5d309eabb0cdb311e535cd1ae0917
Mingjaam <kimminjae0627.kakao.com>
2026-04-13 12:00:17 +0900

    Initial Commit

---
+++ TalkingRoom/TalkingRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkingRoomApp.swift
+//  TalkingRoom
+//
+//  Created by 김민재 on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct TalkingRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TalkingRoomTests/TalkingRoomTests.swift
@@ -0,0 +1,19 @@
+//
+//  TalkingRoomTests.swift
+//  TalkingRoomTests
+//
+//  Created by 김민재 on 4/13/26.
+//
+
+import Testing
+@testable import TalkingRoom
+
+struct TalkingRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
